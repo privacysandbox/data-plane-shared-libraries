4c031b1a9a8b23b1e4b4bba0633237be0dad8935
Privacy Sandbox Team <no-reply@google.com>
2023-03-28 01:52:09 -0700

    Remove BHTTP code references.
    Chrome and Android both will not encode requests using binary HTTP.
    
    Test: bazel test //src/cpp/communication:all --test_output=all --cache_test_results=no --runs_per_test=100
    
    Bug: b/273287080
    Change-Id: I223a109699312ec6a7ffdd1c90337052dff53108
    GitOrigin-RevId: 01f855bc7446ae1beb87c281b24d1b2fae0cd025

---
+++ src/cpp/communication/json_utils.h
@@ -0,0 +1,79 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef SRC_CPP_COMMUNICATION_JSON_UTILS_H_
+#define SRC_CPP_COMMUNICATION_JSON_UTILS_H_
+
+#include <string>
+#include <string_view>
+#include <utility>
+
+#include "absl/status/statusor.h"
+#include "glog/logging.h"
+#include "google/protobuf/util/json_util.h"
+
+namespace privacy_sandbox::server_common {
+
+// Converts JSON string to proto. JsonToProto() is meant for converting client
+// requests to a server defined proto.
+//
+// InvalidArgumentError will be returned if the JSON is malformed or cannot be
+// converted to the specified proto, implying the client sent bad request.
+template <typename ProtoMessage>
+absl::StatusOr<ProtoMessage> JsonToProto(absl::string_view json) {
+  static_assert(std::is_base_of<google::protobuf::Message, ProtoMessage>::value,
+                "JsonToProto only decodes to protobuf messages.");
+
+  ProtoMessage result;
+  google::protobuf::util::JsonParseOptions options;
+  options.ignore_unknown_fields = true;
+
+  if (const auto s =
+          google::protobuf::util::JsonStringToMessage(json, &result, options);
+      !s.ok()) {
+    // protobuf begins to use absl::Status since
+    // https://github.com/protocolbuffers/protobuf/commit/a3c8e2deb05186334b7ee8c1174f44802e38b43d
+    // Once that is available we can simply return the status.
+    // So we should not spend too much time dealing with the conversion now.
+    return absl::InvalidArgumentError(s.message().ToString());
+  }
+
+  return result;
+}
+
+// Converts a proto to a JSON string.
+template <typename ProtoMessage>
+absl::StatusOr<std::string> ProtoToJson(const ProtoMessage& proto) {
+  static_assert(std::is_base_of<google::protobuf::Message, ProtoMessage>::value,
+                "ProtoToJson only encodes from protobuf messages.");
+
+  std::string body;
+  google::protobuf::util::JsonOptions options;
+  options.add_whitespace = false;
+  if (const auto s =
+          google::protobuf::util::MessageToJsonString(proto, &body, options);
+      !s.ok()) {
+    // protobuf begins to use absl::Status since
+    // https://github.com/protocolbuffers/protobuf/commit/a3c8e2deb05186334b7ee8c1174f44802e38b43d
+    // Once that is available we can simply return the status.
+    // So we should not spend too much time dealing with the conversion now.
+    return absl::InternalError(s.message().ToString());
+  }
+
+  return body;
+}
+
+}  // namespace privacy_sandbox::server_common
+
+#endif  // SRC_CPP_COMMUNICATION_JSON_UTILS_H_
