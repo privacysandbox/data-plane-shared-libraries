2382c05c4ca7d90201d82497894c2e01bcc639a7
Privacy Sandbox Team <no-reply@google.com>
2023-11-03 00:19:15 +0000

    fix: Provide a way to override /etc/resolv.conf in proxify
    Bug: b/299461948, b/309019935
    Change-Id: I7ab96ac6ce5a266ca80e8dfb3229625d1a1c17ad
    GitOrigin-RevId: 85545790ede0122c2dc484d19f961a8327750999

--- scp/cc/aws/proxy/src/proxify.cc
+++ scp/cc/aws/proxy/src/proxify.cc
@@ -20,16 +20,22 @@
 
 #include <linux/limits.h>
 
+#include <cstring>
 #include <iostream>
 #include <memory>
 #include <string>
+#include <string_view>
 
-static constexpr char lib[] = "libproxy_preload.so";
-static constexpr char socket_vendor[] = "socket_vendor";
-static constexpr char resolv_conf_path[] = "/etc/resolv.conf";
-static constexpr char use_vc_option[] = "use-vc";
+#include "absl/strings/str_cat.h"
 
-static const char resolv_conf_content[] = R"resolv(
+constexpr std::string_view kLib = "libproxy_preload.so";
+constexpr std::string_view kSocketVendor = "socket_vendor";
+constexpr std::string_view kResolvConfPath = "/etc/resolv.conf";
+constexpr std::string_view kResolvOverrideConfPath =
+    "/etc/resolv_override.conf";
+constexpr std::string_view kUseVcOption = "use-vc";
+
+constexpr std::string_view kResolvConfContent = R"resolv(
 ; use-vc forces use of TCP for DNS resolutions.
 ; See https://man7.org/linux/man-pages/man5/resolv.conf.5.html
 options use-vc timeout:2 attempts:5
@@ -38,6 +44,7 @@ search ec2.internal
 ; See https://docs.aws.amazon.com/vpc/latest/userguide/vpc-dns.html#AmazonDNS
 nameserver 10.0.0.2
 )resolv";
+constexpr size_t kResolvConfContentLength = kResolvConfContent.length() - 1;
 
 int main(int argc, char* argv[]) {
   std::string exe = argv[0];
@@ -50,8 +57,8 @@ int main(int argc, char* argv[]) {
   std::string proc_exe_path =
       std::string("/proc/") + std::to_string(my_pid) + "/exe";
   auto my_path = std::make_unique<char[]>(PATH_MAX);
-  ssize_t sz = readlink(proc_exe_path.c_str(), my_path.get(), PATH_MAX);
-  if (sz < 0) {
+  if (ssize_t sz = readlink(proc_exe_path.c_str(), my_path.get(), PATH_MAX);
+      sz < 0) {
     std::cerr << "ERROR: cannot access " << proc_exe_path << ": "
               << strerror(errno) << std::endl;
     return -1;
@@ -59,13 +66,14 @@ int main(int argc, char* argv[]) {
   // Get the dir name of the executable.
   std::string dir_name = dirname(my_path.get());
   // Here we assume the preload lib is in the same directory.
-  std::string lib_path = dir_name + "/" + lib;
+  std::string lib_path = absl::StrCat(dir_name, "/", kLib.data());
   if (access(lib_path.c_str(), F_OK) != 0) {
     std::cerr << "ERROR: Cannot access " << lib_path << ": " << strerror(errno)
               << std::endl;
     return -1;
   }
-  std::string socket_vendor_path = dir_name + "/" + socket_vendor;
+  std::string socket_vendor_path =
+      absl::StrCat(dir_name, "/", kSocketVendor.data());
   if (access(socket_vendor_path.c_str(), F_OK) != 0) {
     std::cerr << "ERROR: Cannot access " << socket_vendor_path << ": "
               << strerror(errno) << std::endl;
@@ -83,29 +91,44 @@ int main(int argc, char* argv[]) {
               << std::endl;
     return -1;
   }
-  if (setenv("RES_OPTIONS", use_vc_option, 1)) {
+  if (setenv("RES_OPTIONS", kUseVcOption.data(), 1)) {
     std::cerr << "ERROR: cannot set RES_OPTIONS: " << strerror(errno)
               << std::endl;
     return -1;
   }
   // Before we execute, we need to make sure /etc/resolv.conf exists.
-  if (access(resolv_conf_path, F_OK) != 0) {
+  if (access(kResolvConfPath.data(), F_OK) != 0) {
     // We cannot access /etc/resolv.conf, create one. resolv.conf may be a
     // dangling symlink, unlink it first.
-    unlink(resolv_conf_path);
+    unlink(kResolvConfPath.data());
     // Create and open file for writing with mode 0644.
-    int f = open(resolv_conf_path, O_CREAT | O_EXCL | O_WRONLY,
+    int f = open(kResolvConfPath.data(), O_CREAT | O_EXCL | O_WRONLY,
                  S_IWUSR | S_IRUSR | S_IRGRP | S_IROTH);
     if (f < 0) {
       std::cerr << "ERROR: cannot open /etc/resolv.conf, and cannot create it."
                 << std::endl;
       return -1;
     }
-    // Write the file, excluding the null terminator at end.
-    ssize_t r = write(f, resolv_conf_content, sizeof(resolv_conf_content) - 1);
-    if (r != static_cast<ssize_t>(sizeof(resolv_conf_content) - 1)) {
-      std::cerr << "ERROR: cannot fully write /etc/resolv.conf." << std::endl;
-      return -1;
+    // If the kResolvOverrideConfPath file does not exist, write to
+    // kResolvConfPath. Else, create a symlink kResolvOverrideConfPath ->
+    // kResolvConfPath overriding the kResolvConfPath contents.
+    if (access(kResolvOverrideConfPath.data(), F_OK) != 0) {
+      // Write the file, excluding the null terminator at end.
+      if (const ssize_t r =
+              write(f, kResolvConfContent.data(), kResolvConfContentLength);
+          r != kResolvConfContentLength) {
+        std::cerr << "ERROR: cannot fully write /etc/resolv.conf." << std::endl;
+        return -1;
+      }
+    } else {
+      if (remove(kResolvConfPath.data()) < 0) {
+        std::cerr << "ERROR: could not remove /etc/resolv.conf." << std::endl;
+        return -1;
+      }
+      if (link(kResolvOverrideConfPath.data(), kResolvConfPath.data()) < 0) {
+        std::cerr << "ERROR: could not link /etc/resolv.conf." << std::endl;
+        return -1;
+      }
     }
     close(f);
   }
