655a18f02b9a01c5b4a231ef1efabe17a7a5eb87
Privacy Sandbox Team <no-reply@google.com>
2023-11-17 17:09:38 +0000

    Merge "chore: Improve proxify readability" into main
    GitOrigin-RevId: 97911e278323ae67e1d1bc5250d91ea18a5332bb

--- scp/cc/aws/proxy/src/proxify.cc
+++ scp/cc/aws/proxy/src/proxify.cc
@@ -20,6 +20,7 @@
 
 #include <linux/limits.h>
 
+#include <array>
 #include <cstring>
 #include <iostream>
 #include <memory>
@@ -27,6 +28,8 @@
 #include <string_view>
 
 #include "absl/strings/str_cat.h"
+
+namespace {
 
 constexpr std::string_view kLib = "libproxy_preload.so";
 constexpr std::string_view kSocketVendor = "socket_vendor";
@@ -42,51 +45,46 @@ options use-vc timeout:2 attempts:5
 search ec2.internal
 ; Ip of AWS DNS resolver located at base of VPC IPV4 network range, plus two.
 ; See https://docs.aws.amazon.com/vpc/latest/userguide/vpc-dns.html#AmazonDNS
-nameserver 10.0.0.2
-)resolv";
-constexpr size_t kResolvConfContentLength = kResolvConfContent.length() - 1;
+nameserver 10.0.0.2)resolv";
+
+}  // namespace
 
 int main(int argc, char* argv[]) {
-  std::string exe = argv[0];
   if (argc < 2) {
-    std::cerr << "Usage: " << exe << " app_to_execute" << std::endl;
+    std::cerr << "Usage: proxify <app_to_execute>" << std::endl;
     return -1;
   }
-  // Get current process's executable file path by reading link /proc/[pid]/exe
-  int my_pid = getpid();
-  std::string proc_exe_path =
-      std::string("/proc/") + std::to_string(my_pid) + "/exe";
-  auto my_path = std::make_unique<char[]>(PATH_MAX);
-  if (ssize_t sz = readlink(proc_exe_path.c_str(), my_path.get(), PATH_MAX);
-      sz < 0) {
-    std::cerr << "ERROR: cannot access " << proc_exe_path << ": "
-              << strerror(errno) << std::endl;
-    return -1;
-  }
-  // Get the dir name of the executable.
-  std::string dir_name = dirname(my_path.get());
-  // Here we assume the preload lib is in the same directory.
-  std::string lib_path = absl::StrCat(dir_name, "/", kLib.data());
-  if (access(lib_path.c_str(), F_OK) != 0) {
-    std::cerr << "ERROR: Cannot access " << lib_path << ": " << strerror(errno)
+
+  // Get current process's executable file path by reading link /proc/[pid]/exe.
+  std::array<char, PATH_MAX> proxify_path;
+  if (const std::string link_path = absl::StrCat("/proc/", getpid(), "/exe");
+      readlink(link_path.c_str(), proxify_path.data(), PATH_MAX) < 0) {
+    std::cerr << "ERROR: cannot access " << link_path << ": " << strerror(errno)
               << std::endl;
     return -1;
   }
-  std::string socket_vendor_path =
-      absl::StrCat(dir_name, "/", kSocketVendor.data());
-  if (access(socket_vendor_path.c_str(), F_OK) != 0) {
+  const std::string_view proxify_dir = dirname(proxify_path.data());
+
+  // Assume the preload lib is in the same directory.
+  if (const std::string socket_vendor_path =
+          absl::StrCat(proxify_dir, "/", kSocketVendor);
+      access(socket_vendor_path.c_str(), F_OK) != 0) {
     std::cerr << "ERROR: Cannot access " << socket_vendor_path << ": "
               << strerror(errno) << std::endl;
     return -1;
-  }
-  // Run the socket_vendor. If there's already a running socket_vendor, this
-  // will end with a benign failure.
-  if (fork() == 0) {
+  } else if (fork() == 0) {
+    // Fork and run the socket_vendor. If there's already a running
+    // socket_vendor, this will end with a benign failure.
     daemon(1, 0);
     execl(socket_vendor_path.c_str(), socket_vendor_path.c_str(), nullptr);
-    exit(1);
+    return -1;
   }
-  if (setenv("LD_PRELOAD", lib_path.c_str(), 1)) {
+  if (const std::string lib_path = absl::StrCat(proxify_dir, "/", kLib);
+      access(lib_path.c_str(), F_OK) != 0) {
+    std::cerr << "ERROR: Cannot access " << lib_path << ": " << strerror(errno)
+              << std::endl;
+    return -1;
+  } else if (setenv("LD_PRELOAD", lib_path.c_str(), 1)) {
     std::cerr << "ERROR: cannot set LD_PRELOAD: " << strerror(errno)
               << std::endl;
     return -1;
@@ -96,44 +94,43 @@ int main(int argc, char* argv[]) {
               << std::endl;
     return -1;
   }
-  // Before we execute, we need to make sure /etc/resolv.conf exists.
+
+  // Ensure /etc/resolv.conf exists.
   if (access(kResolvConfPath.data(), F_OK) != 0) {
     // We cannot access /etc/resolv.conf, create one. resolv.conf may be a
     // dangling symlink, unlink it first.
     unlink(kResolvConfPath.data());
+
     // Create and open file for writing with mode 0644.
-    int f = open(kResolvConfPath.data(), O_CREAT | O_EXCL | O_WRONLY,
-                 S_IWUSR | S_IRUSR | S_IRGRP | S_IROTH);
+    const int f = open(kResolvConfPath.data(), O_CREAT | O_EXCL | O_WRONLY,
+                       S_IWUSR | S_IRUSR | S_IRGRP | S_IROTH);
     if (f < 0) {
       std::cerr << "ERROR: cannot open /etc/resolv.conf, and cannot create it."
                 << std::endl;
       return -1;
     }
+
     // If the kResolvOverrideConfPath file does not exist, write to
     // kResolvConfPath. Else, create a symlink kResolvOverrideConfPath ->
     // kResolvConfPath overriding the kResolvConfPath contents.
-    if (access(kResolvOverrideConfPath.data(), F_OK) != 0) {
-      // Write the file, excluding the null terminator at end.
-      if (const ssize_t r =
-              write(f, kResolvConfContent.data(), kResolvConfContentLength);
-          r != kResolvConfContentLength) {
-        std::cerr << "ERROR: cannot fully write /etc/resolv.conf." << std::endl;
-        return -1;
-      }
-    } else {
-      if (remove(kResolvConfPath.data()) < 0) {
-        std::cerr << "ERROR: could not remove /etc/resolv.conf." << std::endl;
-        return -1;
-      }
-      if (link(kResolvOverrideConfPath.data(), kResolvConfPath.data()) < 0) {
-        std::cerr << "ERROR: could not link /etc/resolv.conf." << std::endl;
-        return -1;
-      }
+    if (access(kResolvOverrideConfPath.data(), F_OK) != 0 &&
+        write(f, kResolvConfContent.data(), kResolvConfContent.size()) == -1) {
+      std::cerr << "ERROR: cannot fully write /etc/resolv.conf." << std::endl;
+      return -1;
+    } else if (remove(kResolvConfPath.data()) < 0) {
+      std::cerr << "ERROR: could not remove /etc/resolv.conf." << std::endl;
+      return -1;
+    } else if (link(kResolvOverrideConfPath.data(), kResolvConfPath.data()) <
+               0) {
+      std::cerr << "ERROR: could not link /etc/resolv.conf." << std::endl;
+      return -1;
     }
     close(f);
   }
+
   // Execute!
   execvp(argv[1], &argv[1]);
+
   // If execution reaches here, above call has failed.
   std::cerr << "ERROR: cannot execute " << argv[1] << ": " << strerror(errno)
             << std::endl;
