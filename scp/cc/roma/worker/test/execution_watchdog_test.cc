1a7cc8447570b42851cd760d9793d5e19b03e488
Privacy Sandbox Team <no-reply@google.com>
2023-12-28 18:01:51 +0000

    Revert "fix: Reenable execution watchdog test"
    This reverts commit 8792a3f52021b20f4cab85613d6b8c54e49ca588.
    
    Reason for revert: failing continuous build
    
    Bug: b/309509915
    Change-Id: Ie72edf6d79ecfc7d714e58f5abc84ef0b49ea6e5
    GitOrigin-RevId: d97f7ccc5045820a77ead61cb0da9f4f5e81479f

--- scp/cc/roma/worker/test/execution_watchdog_test.cc
+++ scp/cc/roma/worker/test/execution_watchdog_test.cc
@@ -73,25 +73,27 @@ TEST_F(ExecutionWatchdogTest, StopDoesntTerminate) {
   ASSERT_FALSE(watch_dog_.IsTerminateCalled());
 }
 
-TEST_F(ExecutionWatchdogTest, TerminateOnTimeoutStartTimerAfterRun) {
+// TODO: b/309509915 - Enable test once issue is solved.
+TEST_F(ExecutionWatchdogTest, DISABLED_TerminateOnTimeoutStartTimerAfterRun) {
   watch_dog_.Run();
   constexpr absl::Duration duration = absl::Milliseconds(10);
   watch_dog_.StartTimer(isolate_, duration);
   ASSERT_FALSE(watch_dog_.IsTerminateCalled());
-  absl::SleepFor(absl::Milliseconds(1));
+  absl::SleepFor(duration / 2);
   ASSERT_FALSE(watch_dog_.IsTerminateCalled());
-  absl::SleepFor(duration + absl::Milliseconds(5));
+  absl::SleepFor(duration / 2 + absl::Milliseconds(1));
   ASSERT_TRUE(watch_dog_.IsTerminateCalled());
   watch_dog_.Stop();
 }
 
-TEST_F(ExecutionWatchdogTest, TerminateOnTimeoutStartTimerBeforeRun) {
+// TODO: b/309509915 - Enable test once issue is solved.
+TEST_F(ExecutionWatchdogTest, DISABLED_TerminateOnTimeoutStartTimerBeforeRun) {
   constexpr absl::Duration duration = absl::Milliseconds(10);
   watch_dog_.StartTimer(isolate_, duration);
   watch_dog_.Run();
-  absl::SleepFor(absl::Milliseconds(1));
+  absl::SleepFor(duration / 2);
   ASSERT_FALSE(watch_dog_.IsTerminateCalled());
-  absl::SleepFor(duration + absl::Milliseconds(5));
+  absl::SleepFor(duration / 2 + absl::Milliseconds(1));
   ASSERT_TRUE(watch_dog_.IsTerminateCalled());
   watch_dog_.Stop();
 }
