f5f53f3dc6743e0c3db900a6af670131d327de6e
Privacy Sandbox Team <no-reply@google.com>
2024-03-25 14:41:00 -0400

    test: Add test case for large strings in JS code
    Bug: b/342586575
    Change-Id: I335e296ac3a0b0d1510fd70e4c0aba70f459dddf
    GitOrigin-RevId: ed3da076eee91578df4d5af98463756bb441f520

---
+++ src/roma/roma_service/string_size_test.cc
@@ -0,0 +1,119 @@
+/*
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+#include <thread>
+#include <tuple>
+#include <utility>
+#include <vector>
+
+#include "absl/base/thread_annotations.h"
+#include "absl/strings/str_cat.h"
+#include "absl/synchronization/mutex.h"
+#include "absl/synchronization/notification.h"
+#include "absl/time/time.h"
+#include "src/roma/config/config.h"
+#include "src/roma/config/function_binding_object_v2.h"
+#include "src/roma/interface/roma.h"
+#include "src/roma/native_function_grpc_server/test_request_handlers.h"
+#include "src/roma/roma_service/roma_service.h"
+#include "src/util/duration.h"
+
+using ::google::scp::roma::FunctionBindingPayload;
+using ::google::scp::roma::sandbox::roma_service::RomaService;
+using ::testing::_;
+using ::testing::HasSubstr;
+using ::testing::IsEmpty;
+using ::testing::StrEq;
+
+namespace google::scp::roma::test {
+namespace {
+
+class StringSizeTest : public testing::TestWithParam<size_t> {};
+
+constexpr size_t kStringSizes[] = {
+    100'000,
+    2'000'000,
+    10'000'000,
+    100'000'000,
+};
+
+INSTANTIATE_TEST_SUITE_P(StringSizeTestSuite, StringSizeTest,
+                         testing::ValuesIn(kStringSizes),
+                         testing::PrintToStringParamName());
+
+TEST_P(StringSizeTest, LoadCodeObj) {
+  Config config;
+  config.number_of_workers = 1;
+  RomaService<> roma_service(std::move(config));
+  ASSERT_TRUE(roma_service.Init().ok());
+
+  std::string result;
+
+  const size_t strsize = GetParam();
+  auto code_obj = std::make_unique<CodeObject>(CodeObject{
+      .id = "foo",
+      .version_string = "v1",
+      .js = absl::StrCat("const str = `", std::string(strsize, '.'), "`;",
+                         R"JSCODE(
+    function Handler() {
+      return `some string of length: ${str.length}`;
+    })JSCODE"),
+  });
+
+  absl::Notification load_finished;
+  EXPECT_TRUE(roma_service
+                  .LoadCodeObj(std::move(code_obj),
+                               [&](absl::StatusOr<ResponseObject> resp) {
+                                 EXPECT_TRUE(resp.ok());
+                                 load_finished.Notify();
+                               })
+                  .ok());
+  ASSERT_TRUE(load_finished.WaitForNotificationWithTimeout(absl::Seconds(40)));
+
+  auto execution_obj =
+      std::make_unique<InvocationStrRequest<>>(InvocationStrRequest<>{
+          .id = "foo",
+          .version_string = "v1",
+          .handler_name = "Handler",
+      });
+
+  absl::Notification execute_finished;
+  EXPECT_TRUE(roma_service
+                  .Execute(std::move(execution_obj),
+                           [&execute_finished,
+                            &strsize](absl::StatusOr<ResponseObject> resp) {
+                             EXPECT_TRUE(resp.ok()) << resp.status();
+                             EXPECT_THAT(
+                                 resp->resp,
+                                 StrEq(absl::StrCat("\"some string of length: ",
+                                                    strsize, "\"")));
+                             execute_finished.Notify();
+                           })
+                  .ok());
+  ASSERT_TRUE(
+      execute_finished.WaitForNotificationWithTimeout(absl::Seconds(40)));
+  EXPECT_TRUE(roma_service.Stop().ok());
+}
+
+}  // namespace
+}  // namespace google::scp::roma::test
