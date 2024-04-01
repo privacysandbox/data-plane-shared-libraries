b75948c26e438aa96353d7ae4c1f0f73a5817dea
Privacy Sandbox Team <no-reply@google.com>
2024-04-01 19:48:19 +0000

    chore: Make PrivateKeyClient options into simple struct
    Bug: b/332355046
    Change-Id: I18f2bc41e6ace4477317fb87ae09e4c2aea470a2
    GitOrigin-RevId: 2be3abd3a1b9e224c7a2ff4ec0da236546140fc2

--- src/public/cpio/interface/private_key_client/type_def.h
+++ src/public/cpio/interface/private_key_client/type_def.h
@@ -29,8 +29,6 @@ using GcpWIPProvider = std::string;
 
 /// Configurations for private key vending endpoint.
 struct PrivateKeyVendingEndpoint {
-  virtual ~PrivateKeyVendingEndpoint() = default;
-
   /** @brief The account identity to access the cloud. This is used to create
    *  temporary credentials to access resources you normally has no access. In
    *  AWS, it is the IAM Role ARN. In GCP, it would be the service account.
