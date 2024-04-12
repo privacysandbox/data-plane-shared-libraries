7a933d57327c9c5f2a188fabb74acde8d588a9d9
Privacy Sandbox Team <no-reply@google.com>
2024-04-12 19:09:24 +0000

    feat: Add v8_compiler_benchmark
    Add benchmarks comparing loading and execution of hello world and prime sieve example code with different combinations of the optimizing compilers
    
    ------------------------------------------------------------------------
    Benchmark                              Time             CPU   Iterations
    ------------------------------------------------------------------------
    BM_LoadCodeObjHelloWorld/0          7.85 ms        0.032 ms         1000 --no-turbofan
    BM_LoadCodeObjHelloWorld/1          7.81 ms        0.029 ms         1000 --turboshaft
    BM_LoadCodeObjHelloWorld/2          7.81 ms        0.033 ms         1000 --maglev
    BM_LoadCodeObjHelloWorld/3          8.02 ms        0.037 ms         1000 --maglev --turboshaft
    BM_LoadCodeObjHelloWorld/4          8.20 ms        0.036 ms         1000 --turbofan
    BM_LoadCodeObjHelloWorld/5          7.81 ms        0.031 ms         1000 --turbofan --turboshaft
    BM_LoadCodeObjHelloWorld/6          8.07 ms        0.036 ms         1000 --turbofan --maglev
    BM_LoadCodeObjHelloWorld/7          7.79 ms        0.032 ms         1000 --turbofan --maglev --turboshaft
    BM_LoadCodeObjPrimeSieve/0          7.75 ms        0.032 ms         1000 --no-turbofan
    BM_LoadCodeObjPrimeSieve/1          7.81 ms        0.031 ms         1000 --turboshaft
    BM_LoadCodeObjPrimeSieve/2          7.75 ms        0.029 ms         1000 --maglev
    BM_LoadCodeObjPrimeSieve/3          7.81 ms        0.027 ms         1000 --maglev --turboshaft
    BM_LoadCodeObjPrimeSieve/4          7.71 ms        0.034 ms         1000 --turbofan
    BM_LoadCodeObjPrimeSieve/5          7.76 ms        0.031 ms         1000 --turbofan --turboshaft
    BM_LoadCodeObjPrimeSieve/6          7.87 ms        0.033 ms         1000 --turbofan --maglev
    BM_LoadCodeObjPrimeSieve/7          7.83 ms        0.032 ms         1000 --turbofan --maglev --turboshaft
    BM_ExecuteCodeObjHelloWorld/0      0.577 ms        0.020 ms        10000 --no-turbofan
    BM_ExecuteCodeObjHelloWorld/1      0.589 ms        0.020 ms        10000 --turboshaft
    BM_ExecuteCodeObjHelloWorld/2      0.590 ms        0.019 ms        10000 --maglev
    BM_ExecuteCodeObjHelloWorld/3      0.578 ms        0.020 ms        10000 --maglev --turboshaft
    BM_ExecuteCodeObjHelloWorld/4      0.569 ms        0.019 ms        10000 --turbofan
    BM_ExecuteCodeObjHelloWorld/5      0.580 ms        0.020 ms        10000 --turbofan --turboshaft
    BM_ExecuteCodeObjHelloWorld/6      0.586 ms        0.020 ms        10000 --turbofan --maglev
    BM_ExecuteCodeObjHelloWorld/7      0.583 ms        0.022 ms        10000 --turbofan --maglev --turboshaft
    BM_ExecuteCodeObjPrimeSieve/0       6.17 ms        0.032 ms         1000 --no-turbofan
    BM_ExecuteCodeObjPrimeSieve/1       5.10 ms        0.032 ms         1000 --turboshaft
    BM_ExecuteCodeObjPrimeSieve/2       5.28 ms        0.031 ms         1000 --maglev
    BM_ExecuteCodeObjPrimeSieve/3       4.94 ms        0.029 ms         1000 --maglev --turboshaft
    BM_ExecuteCodeObjPrimeSieve/4       5.22 ms        0.031 ms         1000 --turbofan
    BM_ExecuteCodeObjPrimeSieve/5       5.03 ms        0.032 ms         1000 --turbofan --turboshaft
    BM_ExecuteCodeObjPrimeSieve/6       5.13 ms        0.030 ms         1000 --turbofan --maglev
    BM_ExecuteCodeObjPrimeSieve/7       5.01 ms        0.031 ms         1000 --turbofan --maglev --turboshaft
    
    Bug: b/335236406
    Change-Id: I2d12aa653f2f3f1ed5bf7ca22dc2d79914926274
    GitOrigin-RevId: b5d1cf49c7f31a8ff9d5de6bfa85a961f943c9e4

---
+++ src/roma/benchmark/v8_compiler_benchmark.cc
@@ -0,0 +1,196 @@
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Example command to run this (the grep is necessary to avoid noisy log
+ * output):
+ *
+ * builders/tools/bazel-debian run \
+ * //src/roma/benchmark:logging_benchmark \
+ * --test_output=all 2>&1 | fgrep -v sandbox2.cc
+ */
+
+#include <memory>
+#include <string>
+#include <string_view>
+
+#include <benchmark/benchmark.h>
+
+#include "absl/log/log.h"
+#include "absl/strings/str_cat.h"
+#include "absl/strings/str_join.h"
+#include "absl/synchronization/notification.h"
+#include "src/roma/benchmark/test_code.h"
+#include "src/roma/config/config.h"
+#include "src/roma/interface/roma.h"
+#include "src/roma/roma_service/roma_service.h"
+
+namespace {
+
+using google::scp::roma::CodeObject;
+using google::scp::roma::Config;
+using google::scp::roma::DefaultMetadata;
+using google::scp::roma::InvocationStrRequest;
+using google::scp::roma::ResponseObject;
+using google::scp::roma::benchmark::kCodeHelloWorld;
+using google::scp::roma::benchmark::kCodePrimeSieve;
+using google::scp::roma::benchmark::kHandlerNameHelloWorld;
+using google::scp::roma::benchmark::kHandlerNamePrimeSieve;
+using google::scp::roma::sandbox::roma_service::RomaService;
+
+constexpr auto kTimeout = absl::Seconds(10);
+
+std::vector<std::vector<std::string>> CreateOptimizerCombinations();
+const std::vector<std::string> kOptimizerOpts = {
+    "--turbofan",
+    "--maglev",
+    "--turboshaft",
+};
+const std::vector<std::vector<std::string>> kOptimizerCombos =
+    CreateOptimizerCombinations();
+
+std::unique_ptr<RomaService<>> roma_service;
+
+void DoSetup(typename RomaService<>::Config config) {
+  roma_service.reset(new RomaService<>(std::move(config)));
+  CHECK_OK(roma_service->Init());
+}
+
+void DoTeardown(const benchmark::State& state) {
+  CHECK_OK(roma_service->Stop());
+  roma_service.reset();
+}
+
+void LoadCodeObj(std::string_view code) {
+  absl::Notification load_finished;
+
+  absl::Status load_status = roma_service->LoadCodeObj(
+      std::make_unique<CodeObject>(CodeObject{
+          .id = "foo",
+          .version_string = "v1",
+          .js = std::string(code),
+      }),
+      [&load_finished](const absl::StatusOr<ResponseObject>& resp) {
+        CHECK(resp.ok());
+        load_finished.Notify();
+      });
+
+  CHECK(load_status.ok()) << load_status;
+  CHECK(load_finished.WaitForNotificationWithTimeout(kTimeout));
+}
+
+void LoadCodeBenchmark(benchmark::State& state, std::string_view code) {
+  for (auto _ : state) {
+    LoadCodeObj(code);
+  }
+
+  std::string label = absl::StrJoin(kOptimizerCombos[state.range(0)], " ");
+  state.SetLabel(label.empty() ? "default" : label);
+}
+
+void ExecuteCodeBenchmark(benchmark::State& state, std::string_view code,
+                          std::string_view handler) {
+  LoadCodeObj(code);
+  for (auto _ : state) {
+    absl::Notification execute_finished;
+
+    auto execution_obj =
+        std::make_unique<InvocationStrRequest<>>(InvocationStrRequest<>{
+            .id = "foo",
+            .version_string = "v1",
+            .handler_name = std::string(handler),
+        });
+    auto execution_status = roma_service->Execute(
+        std::move(execution_obj), [&](absl::StatusOr<ResponseObject> resp) {
+          CHECK(resp.ok());
+          execute_finished.Notify();
+        });
+
+    CHECK(execution_status.ok()) << execution_status;
+    CHECK(execute_finished.WaitForNotificationWithTimeout(kTimeout));
+  }
+
+  std::string label = absl::StrJoin(kOptimizerCombos[state.range(0)], " ");
+  state.SetLabel(label.empty() ? "default" : label);
+}
+
+void SetupWithV8Flags(const benchmark::State& state) {
+  typename RomaService<>::Config config;
+  config.number_of_workers = 2;
+  config.SetV8Flags(kOptimizerCombos[state.range(0)]);
+
+  DoSetup(std::move(config));
+}
+
+void CreateOptimizerCombinationsHelper(
+    const std::vector<std::string>& curr_combination, int index,
+    std::vector<std::vector<std::string>>& combinations) {
+  if (index == kOptimizerOpts.size()) {
+    if (curr_combination.empty()) {
+      combinations.push_back({"--no-turbofan"});
+    } else {
+      combinations.push_back(curr_combination);
+    }
+    return;
+  }
+
+  CreateOptimizerCombinationsHelper(curr_combination, index + 1, combinations);
+  std::vector<std::string> new_combination = curr_combination;
+  new_combination.push_back(kOptimizerOpts[index]);
+  CreateOptimizerCombinationsHelper(new_combination, index + 1, combinations);
+}
+
+std::vector<std::vector<std::string>> CreateOptimizerCombinations() {
+  std::vector<std::vector<std::string>> optimizer_combos;
+  CreateOptimizerCombinationsHelper({}, 0, optimizer_combos);
+  return optimizer_combos;
+}
+
+void BM_LoadCodeObjHelloWorld(benchmark::State& state) {
+  LoadCodeBenchmark(state, kCodeHelloWorld);
+}
+
+void BM_LoadCodeObjPrimeSieve(benchmark::State& state) {
+  LoadCodeBenchmark(state, kCodePrimeSieve);
+}
+
+void BM_ExecuteCodeObjHelloWorld(benchmark::State& state) {
+  ExecuteCodeBenchmark(state, kCodeHelloWorld, kHandlerNameHelloWorld);
+}
+
+void BM_ExecuteCodeObjPrimeSieve(benchmark::State& state) {
+  ExecuteCodeBenchmark(state, kCodePrimeSieve, kHandlerNamePrimeSieve);
+}
+
+BENCHMARK(BM_LoadCodeObjHelloWorld)
+    ->DenseRange(0, kOptimizerCombos.size() - 1)
+    ->Setup(SetupWithV8Flags)
+    ->Teardown(DoTeardown);
+BENCHMARK(BM_LoadCodeObjPrimeSieve)
+    ->DenseRange(0, kOptimizerCombos.size() - 1)
+    ->Setup(SetupWithV8Flags)
+    ->Teardown(DoTeardown);
+BENCHMARK(BM_ExecuteCodeObjHelloWorld)
+    ->DenseRange(0, kOptimizerCombos.size() - 1)
+    ->Setup(SetupWithV8Flags)
+    ->Teardown(DoTeardown);
+BENCHMARK(BM_ExecuteCodeObjPrimeSieve)
+    ->DenseRange(0, kOptimizerCombos.size() - 1)
+    ->Setup(SetupWithV8Flags)
+    ->Teardown(DoTeardown);
+
+}  // namespace
+
+// Run the benchmark
+BENCHMARK_MAIN();
