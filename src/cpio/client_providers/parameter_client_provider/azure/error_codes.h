62911e341572d1195d05535533a0bd68bfceebae
Kapil Vaswani <46554948+kapilvgit@users.noreply.github.com>
2024-05-03 17:47:30 +0000

    cpplint

--- src/cpio/client_providers/parameter_client_provider/azure/error_codes.h
+++ src/cpio/client_providers/parameter_client_provider/azure/error_codes.h
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef CPIO_CLIENT_PROVIDERS_PARAMETER_CLIENT_PROVIDER_SRC_AZURE_ERROR_CODES_H_
-#define CPIO_CLIENT_PROVIDERS_PARAMETER_CLIENT_PROVIDER_SRC_AZURE_ERROR_CODES_H_
+#ifndef CPIO_CLIENT_PROVIDERS_PARAMETER_CLIENT_PROVIDER_AZURE_ERROR_CODES_H_
+#define CPIO_CLIENT_PROVIDERS_PARAMETER_CLIENT_PROVIDER_AZURE_ERROR_CODES_H_
 
 #include "src/core/interface/errors.h"
 #include "src/public/core/interface/execution_result.h"
@@ -38,4 +38,4 @@ MAP_TO_PUBLIC_ERROR_CODE(
 
 }  // namespace google::scp::core::errors
 
-#endif  // CPIO_CLIENT_PROVIDERS_PARAMETER_CLIENT_PROVIDER_SRC_AZURE_ERROR_CODES_H_
+#endif  // CPIO_CLIENT_PROVIDERS_PARAMETER_CLIENT_PROVIDER_AZURE_ERROR_CODES_H_
