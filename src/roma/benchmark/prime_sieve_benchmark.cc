a2e55858c913291b287a1ed1e59a59fd81bfd084
Privacy Sandbox Team <no-reply@google.com>
2024-07-19 01:57:58 +0000

    chore: Add roma benchmark with varying prime count
    --------------------------------------------------------------------
    Benchmark                          Time             CPU   Iterations
    --------------------------------------------------------------------
    BM_PrimeSieveUdf/100000         4.46 ms        0.030 ms         1000
    BM_PrimeSieveUdf/500000         18.9 ms        0.038 ms         1000
    BM_PrimeSieveUdf/1000000        36.6 ms        0.041 ms          100
    BM_PrimeSieveUdf/5000000         182 ms        0.066 ms          100
    BM_PrimeSieveUdf/10000000        370 ms        0.088 ms           10
    
    Bug: b/354204020
    Change-Id: Ifc539e422647a45c2a98345aa888ca19ea4bd8eb
    GitOrigin-RevId: 796513ff969d8cee8d38be25edcb8548db527a01

---
+++ src/roma/benchmark/prime_sieve_benchmark.cc
@@ -0,0 +1,120 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <memory>
+
+#include <benchmark/benchmark.h>
+
+#include "absl/log/check.h"
+#include "absl/log/initialize.h"
+#include "absl/strings/substitute.h"
+#include "absl/synchronization/notification.h"
+#include "src/roma/config/config.h"
+#include "src/roma/interface/roma.h"
+#include "src/roma/roma_service/roma_service.h"
+
+using google::scp::roma::CodeObject;
+using google::scp::roma::Config;
+using google::scp::roma::InvocationStrRequest;
+using google::scp::roma::sandbox::roma_service::RomaService;
+
+namespace {
+void BM_PrimeSieveUdf(::benchmark::State& state) {
+  Config<> config;
+  config.number_of_workers = 2;
+  RomaService<> roma_service(std::move(config));
+  CHECK_OK(roma_service.Init());
+  {
+    absl::Notification done;
+    CHECK_OK(
+        roma_service.LoadCodeObj(std::make_unique<CodeObject>(CodeObject{
+                                     .id = "foo",
+                                     .version_string = "v1",
+                                     .js = absl::Substitute(R"(
+  // Create a boolean array of size n+1
+  buffer = new Array($0 + 1).fill(true);
+
+  function sieve() {
+    // Set first two values to false
+    buffer[0] = false;
+    buffer[1] = false;
+    // Loop through the elements
+    for (let i = 2; i <= Math.sqrt($0); i++) {
+       if (buffer[i]) {
+          for (let j = i * i; j <= $0; j += i) {
+             buffer[j] = false;
+          }
+       }
+    }
+    for (let i = $0; i >= 0; i--) {
+       if (buffer[i]) {
+          return i
+       }
+    }
+    return -1;
+  }
+)",
+                                                            state.range(0)),
+                                 }),
+                                 [&done](auto response) {
+                                   CHECK_OK(response);
+                                   done.Notify();
+                                 }));
+    done.WaitForNotification();
+  }
+  const InvocationStrRequest<> request{
+      .id = "foo",
+      .version_string = "v1",
+      .handler_name = "sieve",
+  };
+  {
+    absl::Notification done;
+    CHECK_OK(roma_service.Execute(
+        std::make_unique<InvocationStrRequest<>>(request),
+        [&done](auto response) {
+          CHECK_OK(response);
+          int largest_prime;
+          CHECK(absl::SimpleAtoi(response->resp, &largest_prime));
+          CHECK_GT(largest_prime, 0);
+          done.Notify();
+        }));
+    done.WaitForNotification();
+  }
+  for (auto _ : state) {
+    absl::Notification done;
+    CHECK_OK(
+        roma_service.Execute(std::make_unique<InvocationStrRequest<>>(request),
+                             [&done](auto response) {
+                               CHECK_OK(response);
+                               done.Notify();
+                             }));
+    done.WaitForNotification();
+  }
+  CHECK_OK(roma_service.Stop());
+}
+}  // namespace
+
+BENCHMARK(BM_PrimeSieveUdf)
+    ->Arg(100'000)
+    ->Arg(500'000)
+    ->Arg(1'000'000)
+    ->Arg(5'000'000)
+    ->Arg(10'000'000);
+
+int main(int argc, char* argv[]) {
+  absl::InitializeLog();
+  benchmark::Initialize(&argc, argv);
+  benchmark::RunSpecifiedBenchmarks();
+  return 0;
+}
