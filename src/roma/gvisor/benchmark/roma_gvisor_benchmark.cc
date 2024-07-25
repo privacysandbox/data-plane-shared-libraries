6e7ec253b2b534840391ecddc203e607acc16299
Privacy Sandbox Team <no-reply@google.com>
2024-07-25 18:10:59 +0000

    feat: Make payload size customizable args
    Bug: b/355261309
    Change-Id: I8af1b24904eda11980bca828360c45fa5c5a71c9
    GitOrigin-RevId: ce0b8dbe597cb2f7335163bec351c5405ed25922

--- src/roma/gvisor/benchmark/roma_gvisor_benchmark.cc
+++ src/roma/gvisor/benchmark/roma_gvisor_benchmark.cc
@@ -399,58 +399,28 @@ BENCHMARK(BM_ExecuteBinaryAsyncUnaryGrpc)
     })
     ->ArgNames({"mode", "udf", "num_pre_warmed_workers"});
 
-BENCHMARK(BM_RequestPayload)
-    // elem count: 1
-    ->Args({1, 1, (int)Mode::kModeGvisor})
-    ->Args({1000, 1, (int)Mode::kModeGvisor})
-    ->Args({5000, 1, (int)Mode::kModeGvisor})
-    ->Args({10'000, 1, (int)Mode::kModeGvisor})
-    ->Args({50'000, 1, (int)Mode::kModeGvisor})
-    ->Args({100'000, 1, (int)Mode::kModeGvisor})
-    ->Args({500'000, 1, (int)Mode::kModeGvisor})
-    ->Args({1'000'000, 1, (int)Mode::kModeGvisor})
-    ->Args({5'000'000, 1, (int)Mode::kModeGvisor})
-    ->Args({50'000'000, 1, (int)Mode::kModeGvisor})
-    // elem count: 10
-    ->Args({1, 10, (int)Mode::kModeGvisor})
-    ->Args({1000, 10, (int)Mode::kModeGvisor})
-    ->Args({5000, 10, (int)Mode::kModeGvisor})
-    ->Args({10'000, 10, (int)Mode::kModeGvisor})
-    ->Args({50'000, 10, (int)Mode::kModeGvisor})
-    ->Args({100'000, 10, (int)Mode::kModeGvisor})
-    ->Args({500'000, 10, (int)Mode::kModeGvisor})
-    ->Args({5'000'000, 10, (int)Mode::kModeGvisor})
-    // elem count: 1000
-    ->Args({1, 1000, (int)Mode::kModeGvisor})
-    ->Args({1000, 1000, (int)Mode::kModeGvisor})
-    ->Args({5000, 1000, (int)Mode::kModeGvisor})
-    ->Args({50'000, 1000, (int)Mode::kModeGvisor})
-
-    // elem count: 1
-    ->Args({1, 1, (int)Mode::kModeLocal})
-    ->Args({1000, 1, (int)Mode::kModeLocal})
-    ->Args({5000, 1, (int)Mode::kModeLocal})
-    ->Args({10'000, 1, (int)Mode::kModeLocal})
-    ->Args({50'000, 1, (int)Mode::kModeLocal})
-    ->Args({100'000, 1, (int)Mode::kModeLocal})
-    ->Args({500'000, 1, (int)Mode::kModeLocal})
-    ->Args({1'000'000, 1, (int)Mode::kModeLocal})
-    ->Args({5'000'000, 1, (int)Mode::kModeLocal})
-    ->Args({50'000'000, 1, (int)Mode::kModeLocal})
-    // elem count: 10
-    ->Args({1, 10, (int)Mode::kModeLocal})
-    ->Args({1000, 10, (int)Mode::kModeLocal})
-    ->Args({5000, 10, (int)Mode::kModeLocal})
-    ->Args({10'000, 10, (int)Mode::kModeLocal})
-    ->Args({50'000, 10, (int)Mode::kModeLocal})
-    ->Args({100'000, 10, (int)Mode::kModeLocal})
-    ->Args({500'000, 10, (int)Mode::kModeLocal})
-    ->Args({5'000'000, 10, (int)Mode::kModeLocal})
-    // elem count: 1000
-    ->Args({1, 1000, (int)Mode::kModeLocal})
-    ->Args({1000, 1000, (int)Mode::kModeLocal})
-    ->Args({5000, 1000, (int)Mode::kModeLocal})
-    ->Args({50'000, 1000, (int)Mode::kModeLocal});
+static void PayloadArguments(benchmark::internal::Benchmark* b) {
+  constexpr int64_t kMaxPayloadSize = 50'000'000;
+  constexpr int modes[] = {
+      static_cast<int>(Mode::kModeGvisor),
+      static_cast<int>(Mode::kModeLocal),
+  };
+  constexpr int64_t elem_counts[] = {1, 10, 100, 1'000};
+  constexpr int64_t elem_sizes[] = {
+      1,       1'000,   5'000,     10'000,    50'000,
+      100'000, 500'000, 1'000'000, 5'000'000, 50'000'000,
+  };
+  for (auto mode : modes) {
+    for (auto elem_count : elem_counts) {
+      for (auto elem_size : elem_sizes) {
+        if (elem_count * elem_size <= kMaxPayloadSize) {
+          b->Args({elem_size, elem_count, mode});
+        }
+      }
+    }
+  }
+}
+BENCHMARK(BM_RequestPayload)->Apply(PayloadArguments);
 
 int main(int argc, char* argv[]) {
   absl::InitializeLog();
